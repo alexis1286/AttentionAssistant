eb78b1ad8ec98b63b293644162012d39123424e3
PaulFracz <pfrac001@odu.edu>
2022-01-24 23:54:03 -0500

    Adding code to Observer class

--- src/main/java/AttentionAssistant/Observer.java
+++ src/main/java/AttentionAssistant/Observer.java
@@ -2,4 +2,10 @@ package AttentionAssistant;
 
 public class Observer {
 	
+	/**
+	 * Score variables used to determine if user is off topic
+	 */
+	private int mouseMovementsScore, eyeMovementScore, keyBoardScore, osEventsScore, internetScore, observerScore;
+	
+	
 }
