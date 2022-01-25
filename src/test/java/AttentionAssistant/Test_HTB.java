98be238d9b4c05cda7211b711e2e9a90b6bc9703
jmitchel <jmitc001@odu.edu>
2022-01-24 20:31:42 -0500

    Added place holders for FTS, HTB, NTB, PT, & PM

--- src/test/java/AttentionAssistant/Test_HTB.java
+++ src/test/java/AttentionAssistant/Test_HTB.java
@@ -1,6 +1,51 @@
 package AttentionAssistant;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class Test_HTB {
+	Happy_Thought_Button defaultHTB;
+	Happy_Thought_Button nondefaultHTB;
+	Happy_Thought_Button copyHTB;
+	
+	
+	@BeforeEach
+	void setup() {
+	/**
+	 * place holder
+	 */
+	}
+	
+    @Test
+    @DisplayName("<Happy_Thought_Button> Default Constructor")
+    void HappyThoughtButtonDefaultConstructor() {
+    /**
+     * place holder
+     */
+    }
+
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+    @Test
+    @DisplayName("<Happy_Thought_Button> Parameter Constructor")
+    void HappyThoughtButtonParameterConstructor() {
+    /**
+     * place holder
+     */
+    }
+
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+    @Test
+    @DisplayName("<Happy_Thought_Button> Copy Constructor")
+    void HappyThoughtButtonCopyConstructor() {
+    /**
+     * place holder
+     */
+    }
+
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 	
 }
--- src/test/java/AttentionAssistant/Test_Pomodoro_Timer.java
+++ src/test/java/AttentionAssistant/Test_Pomodoro_Timer.java
@@ -1,6 +1,52 @@
 package AttentionAssistant;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class Test_Pomodoro_Timer {
+	Pomodoro_Timer defaultPT;
+	Pomodoro_Timer nondefaultPT;
+	Pomodoro_Timer copyPT;
+	
+	
+	@BeforeEach
+	void setup() {
+	/**
+	 * place holder
+	 */
+	}
+	
+    @Test
+    @DisplayName("<Pomodoro_Timer> Default Constructor")
+    void PomodoroTimerDefaultConstructor() {
+    /**
+     * place holder
+     */
+    }
+
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+    @Test
+    @DisplayName("<Pomodoro_Timer> Parameter Constructor")
+    void PomodoroTimerParameterConstructor() {
+    /**
+     * place holder
+     */
+    }
+
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+    @Test
+    @DisplayName("<Pomodoro_Timer> Copy Constructor")
+    void PomodoroTimerCopyConstructor() {
+    /**
+     * place holder
+     */
+    }
+
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 	
 }
