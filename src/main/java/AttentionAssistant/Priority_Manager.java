6172a021f1f9d7f3b2b9a9addd070555bbb2eb10
averi <averi002@odu.edu>
2022-01-26 16:24:52 -0500

    nav bar basic function, add images folder with icon images

--- src/main/java/AttentionAssistant/Priority_Manager.java
+++ src/main/java/AttentionAssistant/Priority_Manager.java
@@ -1,5 +1,19 @@
 package AttentionAssistant;
 
+import java.awt.*;
+import javax.swing.*;
+
 public class Priority_Manager {
+	//pm pulls task list from file/database on open so 
+	
+	
+	public void open_pm() {
+		JFrame frame = new JFrame();
+	    //frame.setBackground();
+	    frame.setAlwaysOnTop(true);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    //frame.setLocation(x_coord, y_coord);
+	    frame.setVisible(true);
+	}
 	
 }
---
+++ src/main/java/AttentionAssistant/Task.java
@@ -0,0 +1,5 @@
+package AttentionAssistant;
+
+public class Task {
+	
+}
