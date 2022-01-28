2024be693a79e89ad9611a15823bc59892b75fc6
eholster <ehols001@odu.edu>
2022-01-28 16:17:57 -0500

    Started on initial code for OS event tracking

--- src/main/java/AttentionAssistant/OSEventsTracker.java
+++ src/main/java/AttentionAssistant/OSEventsTracker.java
@@ -6,18 +6,53 @@
 
 package AttentionAssistant;
 
+import java.util.*;
+
 public class OSEventsTracker {
 	
-	int osEventsScore = 100;
+	int osEventsScore;
 	
 	/**
 	 * Constructor
 	 */
 	public OSEventsTracker(){
+		osEventsScore = 100;
 	}
 	
-	protected void startTracking() {
-		//update eyeMovementScore every second
+	/**
+	 * Compares the current processID to the taskID every x seconds
+	 * @param comparableID a comparable task id
+	 */
+	public void startTracking(int comparableID) {
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				compareCurrentProcess(comparableID);
+			}
+		}, 0, 5000); //0 ms before taking action, repeat the action every 5000 ms
+		timer.cancel(); //not sure if this is needed or not yet
+	}
+	
+	/**
+	 * ~~~NEED TO FIGURE OUT INFO FROM TASK~~~
+	 * Compares the current process ID to the 
+	 * designated Task process ID 
+	 */
+	public void compareCurrentProcess(int comparableID) {
+		int pid = (int)getCurrentProcessID();
+		if(comparableID != pid) //currently comparableID will always != pid 
+			osEventsScore = 0;
+		else
+			osEventsScore = 100;
+	}
+	
+	/**
+	 * Retrieve the current process ID
+	 * @return current process ID
+	 */
+	public static long getCurrentProcessID() {
+		return ProcessHandle.current().pid();
 	}
 	
 	protected int getOSEventsScore(){
