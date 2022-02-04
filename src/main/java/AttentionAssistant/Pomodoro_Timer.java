894d70a45e0c246f0a7e2b47835bfbdfcc5dcbed
alovitz <alovi004@odu.edu>
2022-02-04 04:20:31 -0500

    updated timer that takes user input

--- src/main/java/AttentionAssistant/Pomodoro_Timer.java
+++ src/main/java/AttentionAssistant/Pomodoro_Timer.java
@@ -7,11 +7,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.awt.Color;
-import java.awt.Dimension;
-import javax.swing.JFrame;
+import java.text.DecimalFormat;
 
 import javax.swing.UIManager.*;
+
+
 
 
 @SuppressWarnings("serial")
@@ -19,14 +19,19 @@ public class Pomodoro_Timer extends JFrame implements ActionListener
 {
 	Color aa_grey = new Color(51,51,51);
 	Color aa_purple = new Color(137,31,191);
-	
+	int breakmin =0,min = 0,sec=0 ,ms=0;
+	boolean chk=false;
+	private JButton lastButtonPressed;  
 
 	public static int count=0,index=0;
 	Container container = getContentPane();
-	JLabel time = new JLabel("00:00");
+	JLabel time = new JLabel("00m:00s");
 	JButton startbut=new JButton("Start");
 	JButton pausebut=new JButton("Pause");
 	JButton endbut=new JButton("Reset");
+	JLabel c=new JLabel("Pomodoro Timer");
+	JLabel b=new JLabel("Break Timer");
+	
 
 	Timer t;
 	public Pomodoro_Timer() {
@@ -38,6 +43,10 @@ public class Pomodoro_Timer extends JFrame implements ActionListener
 		this.getContentPane().setBackground(aa_grey);
 		Initialize();
         addActionEvent();
+    	Maininput();
+		Breakinput();
+        
+		
 		
 	}
 	private void Initialize() {
@@ -69,8 +78,24 @@ public class Pomodoro_Timer extends JFrame implements ActionListener
 		endbut.setForeground(Color.WHITE);
 		endbut.setFont(new Font("San Francisco", Font.BOLD, 15));
 		container.add(endbut);
-	
 		
+		
+		
+		c.setBounds(185,140, 300, 300);
+		c.setForeground(Color.white);
+		c.setFont(new Font("Dosis SemiBold",Font.BOLD,20));
+		c.setHorizontalAlignment(SwingConstants.CENTER);
+		container.add(c);
+
+		b.setBounds(185,140, 300, 300);
+		b.setForeground(Color.white);
+		b.setFont(new Font("Dosis SemiBold",Font.BOLD,20));
+		b.setHorizontalAlignment(SwingConstants.CENTER);
+		container.add(b);
+		
+		c.setVisible(false);
+		b.setVisible(false);
+
 		
 	}
 	
@@ -80,13 +105,78 @@ public class Pomodoro_Timer extends JFrame implements ActionListener
 		startbut.addActionListener(this);
 		endbut.addActionListener(this);
 		pausebut.addActionListener(this);
+	
 		
 	}
-	int sec=0,h=0;
-	boolean chk=false;
-	private JButton lastButtonPressed;  
+	
+	public void Maininput() {
+		int mainTimer = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter desired time in minutes:"));
+		min = mainTimer;
+		
+		if(mainTimer == 0) {
+			JFrame frame = new JFrame();
+			JOptionPane.showMessageDialog(frame, "Invalid input. Please try again.");
+		}
+	}
+	public void Breakinput() {
+		int breakTimer = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter desired break time in minutes:"));
+		breakmin = breakTimer;
+		
+		if(breakTimer == 0) {
+			JFrame frame = new JFrame();
+			JOptionPane.showMessageDialog(frame, "Invalid input. Please try again.");
+		}
+	}
+	
+	
+	
+	public void MainTimer() {
+			
+		c.setVisible(true);
+		t = new Timer(1000, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String  ddsecond,ddminute;
+				DecimalFormat dformat = new DecimalFormat("00");
+				
+				
+				sec--;
+				
+				ddsecond = dformat.format(sec);
+				ddminute = dformat.format(min);
+				time.setText(String.valueOf(ddminute+"m:"+ddsecond+"s"));
+				if(min == 0 && sec==0) {
+					
+					t.stop();
+					c.setVisible(false);
+					BreakTimer();
+				
+				}
+				
+				
+				if(sec == -1) {
+					sec =59;
+					min--;
+					ddsecond = dformat.format(sec);
+					ddminute = dformat.format(min);
+					time.setText(String.valueOf(ddminute+"m:"+ddsecond+"s"));	
+					chk = false;
+					
+				}
+				
+		
+			}
+			
+		});
+
+		t.start();
+
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
+	
 		JButton buttonPressed = (JButton) e.getSource();
 		if(lastButtonPressed == buttonPressed)
 		{
@@ -96,45 +186,79 @@ public class Pomodoro_Timer extends JFrame implements ActionListener
 		else
 		{
 			if(e.getSource()==startbut) {
-				chk=true;
-				t=new Timer(10, new ActionListener() {
-					
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						time.setText(String.valueOf(h+"m:"+sec+"s"));
-						sec++;
-					
-						
-						if(sec==60) {
-							h++;
-							sec=0;	
-						}
-					}
-					
-				});
+				MainTimer();
+		
+			}
+			if(e.getSource()==pausebut) {
+				if(min == 0 && sec==0) {
+					JFrame frame = new JFrame();
+					JOptionPane.showMessageDialog(frame, "Timer has ended.");
+				}
+				else {
+					t.stop();
+				}
 				
-				t.start();
+			}
+			if(e.getSource()==endbut) {
+				t.stop();
+				sec=min=count=0;
+				time.setText(String.valueOf("00m:00s"));
+				Maininput();
+				Breakinput();
 				
 			}
 			
 		}
-		lastButtonPressed = buttonPressed;
-		if(e.getSource()==endbut) {
-			if(chk==true)t.stop();
-			sec=h=count=0;
-			time.setText(String.valueOf(h+"m:"+sec+"s"));
-		}
-		if(e.getSource()==pausebut) {
-			if(chk==true)t.stop();
-		}
 			
+	}
+		
+	public void BreakTimer() {
+		b.setVisible(true);
+		
+		t = new Timer(1000, new ActionListener() {
 			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String  ddsecond,ddminute;
+				DecimalFormat dformat = new DecimalFormat("00");
+				
+				
+				sec--;
+				
+				ddsecond = dformat.format(sec);
+				ddminute = dformat.format(breakmin);
+				time.setText(String.valueOf(ddminute+"m:"+ddsecond+"s"));
+				if(breakmin == 0 && sec==0) {
+					
+					t.stop();
+				
+				
+				}
+				
+				
+				if(sec == -1) {
+					sec =59;
+					breakmin--;
+					ddsecond = dformat.format(sec);
+					ddminute = dformat.format(breakmin);
+					time.setText(String.valueOf(ddminute+"m:"+ddsecond+"s"));	
+					chk = false;
+					
+				}
+				
 		
-		
+			}
+			
+		});
+
+		t.start();
+
 	}
 	public static void run_pomo()
 	{
+	
 		Pomodoro_Timer tm = new Pomodoro_Timer();
 		tm.setVisible(true);
+		
 	}
 }
