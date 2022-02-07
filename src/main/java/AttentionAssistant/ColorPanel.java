ce43c8ef2aaeba2f195268aa53312ad7b8e71583
averi <averi002@odu.edu>
2022-02-07 02:01:37 -0500

    FTS (program "Java Drawing") edits and integration

---
+++ src/main/java/AttentionAssistant/ColorPanel.java
@@ -0,0 +1,23 @@
+package AttentionAssistant;
+
+/**
+ *   WEEK 8 HIA APPLICATION
+ *   Ivana Zuber
+ *   March 2013
+ *   The University of Liverpool. UK
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+//Class used to set the background of the JPanel
+public class ColorPanel extends JPanel
+{
+    /**************************************************************************************************************
+     *******************************************************CONSTRUCTOR********************************************
+     **************************************************************************************************************/
+    public ColorPanel(Color clr)
+    {
+        setBackground(clr);     //set the background of the JPanel
+    }
+}
