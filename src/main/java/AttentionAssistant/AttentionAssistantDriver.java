b654024f0c4c9c208826e895b787047dd31e04e6
Lexi <76823430+DarthIxel@users.noreply.github.com>
2022-02-07 01:46:28 -0500

    Update AttentionAssistantDriver.java

--- src/main/java/AttentionAssistant/AttentionAssistantDriver.java
+++ src/main/java/AttentionAssistant/AttentionAssistantDriver.java
@@ -2,13 +2,21 @@ package AttentionAssistant;
 
 public class AttentionAssistantDriver {
 	static DataBase db = new DataBase();
+	static Settings settings = new Settings();
+	static Nav_Bar navbar = new Nav_Bar(settings);
+	static Priority_Manager priority_manager = new Priority_Manager();
+	static Pomodoro_Timer pomodoro_timer = new Pomodoro_Timer();
+	static Observer observer = new Observer();
+	static Negative_Thought_Burner negative_thought_burner = new Negative_Thought_Burner();
+	static Happy_Thought_Button happy_thought_button = new Happy_Thought_Button();
+	static Free_Thought_Space free_thought_space = new Free_Thought_Space();
 	
 	public static void main(String[] args) throws Exception {
 		//user login->userID(links db)
 		
 		db.DatabaseSetUp();
 		//get settings
-	    new Nav_Bar(db);
+	   	 Nav_Bar.run_nav_bar(db,navbar,settings,observer,priority_manager,pomodoro_timer,negative_thought_burner,happy_thought_button,free_thought_space);
 		
 		/**
 		 * TEST CODE
