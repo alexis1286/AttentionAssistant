536d6f7c6e6a61ed67f2ebaeffbfb48b1aa01915
eholster <ehols001@odu.edu>
2022-02-17 21:54:38 -0500

    added internet tracker test file

---
+++ src/test/java/AttentionAssistant/Test_InternetTracker.java
@@ -0,0 +1,31 @@
+package AttentionAssistant;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test File for InternetTracker functions.
+ * @author ehols001
+ */
+public class Test_InternetTracker {
+	
+	InternetTracker testIT = new InternetTracker();
+	
+	@BeforeEach
+	void setup() {
+		
+	}
+	
+	@Test
+	@Order(1)
+	@DisplayName("<InternetTracker> placeholder1")
+	void placeholder1() {
+		
+	}
+}
