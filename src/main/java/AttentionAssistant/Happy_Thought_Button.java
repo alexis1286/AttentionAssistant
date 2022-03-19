c7163a8dc4366a009bc4fd76409130aa156672cf
kchristl <kchri011@odu.edu>
2022-03-18 20:34:06 -0400

    Split HTB into new Media Class to handle media objects. Moved HTB JUnits
    test over to Media JUnit tests. Resized images for HTB media.

--- src/main/java/AttentionAssistant/Happy_Thought_Button.java
+++ src/main/java/AttentionAssistant/Happy_Thought_Button.java
@@ -16,7 +16,7 @@ import javax.swing.border.LineBorder;
 /**
  * Class that contains information whenever Happy_Thought_Button
  * is called.
- * @author jmitchel2
+ * @author krchr
  */
 public class Happy_Thought_Button {
 
@@ -40,15 +40,6 @@ public class Happy_Thought_Button {
 	final static boolean shouldWeightX = true; 
 	final static boolean RIGHT_TO_LEFT = false; 
 	
-	/**
-	 * Primary Key for Happy Thought Button
-	 */
-	private int hTB_ID;
-
-	/**
-	 * Media ID for the media displayed from the Happy_Thought_Button
-	 */
-	private String media_ID_Tag;
 	
 	/**
 	 * If the media is flagged by the user or not
@@ -64,22 +55,14 @@ public class Happy_Thought_Button {
 	
 	private int rating;
 	
-	/**
-	 * The Date and Time the Happy_Thought_Button was executed 
-	 */
-	
-	private Date dT_Executed;
 	
 	/**
 	 * Instantiating empty Happy_Thought_Button object
 	 * @author jmitchel2
 	 */
 	public Happy_Thought_Button() {
-		this.hTB_ID= 0;
-		this.media_ID_Tag = "";
 		this.flagged = false;
 		this.rating = 1;
-		this.dT_Executed= null;
 	}
 
 	/**
@@ -89,23 +72,17 @@ public class Happy_Thought_Button {
 	 * @author krchr
 	 * @param int, String, boolean
 	 */
-	public Happy_Thought_Button(int hTB_ID, String media_ID_Tag, boolean flagged, int rating, Date dT_Executed) {
-		this.hTB_ID= hTB_ID;
-		this.media_ID_Tag= media_ID_Tag;
+	public Happy_Thought_Button(boolean flagged, int rating) {
 		this.flagged = flagged;
 		this.rating = rating;
-		this.dT_Executed= dT_Executed;
 	}
 	
 	/**
 	 * Instantiating copy constructor for Happy_Thought_Button object
 	 */
 	public Happy_Thought_Button(Happy_Thought_Button hTB) {
-		this.hTB_ID= hTB.hTB_ID;
-		this.media_ID_Tag = hTB.media_ID_Tag;
 		this.flagged = hTB.flagged;
 		this.rating = hTB.rating;
-		this.dT_Executed= hTB.dT_Executed;
 	}
 	
 	/**
@@ -116,38 +93,7 @@ public class Happy_Thought_Button {
 	 * @author krchr
 	 * @return int
 	 */
-	public int getHTBID() {
-		return this.hTB_ID;
-	}
 	
-	/**
-	 * User should not be able to set the hTB_ID this should be done automatically through the database
-	 * comment out once database is working.
-	 * 
-	 * Set hTB_ID
-	 * @param int
-	 */
-	public void setHTBID(int hTB_ID) {
-		this.hTB_ID= hTB_ID;
-	}
-
-	/**
-	 * Get media_ID_Tag
-	 * 
-	 * @return String
-	 */
-	public String getMedia_ID_Tag() {
-		return this.media_ID_Tag;
-	}
-	
-	/**
-	 * Set media_ID_Tag
-	 * 
-	 * @param String
-	 */
-	public void setMedia_ID_Tag(String media_ID_Tag) {
-		this.media_ID_Tag= media_ID_Tag;
-	}
 
 	/**
 	 * Get flagged
@@ -180,41 +126,6 @@ public class Happy_Thought_Button {
 	public void setRating(int rating) {
 		this.rating= rating;
 	}
-
-	/**
-	 * Get dT_Executed
-	 * 
-	 * @return Date
-	 */
-	public Date getDT_Executed() {
-		return this.dT_Executed;
-	}
-	
-	/**
-	 * Set dT_Executed
-	 * 
-	 * @param Date
-	 */
-	public void setDT_Executed(Date dT_Executed) {
-		this.dT_Executed= dT_Executed;
-	}
-
-	  /** 
-	   * Display HTB
-	   * @return String
-	   */
-	@Override
-	public String toString() {
-	 	String hTBString= new String();
-	 	hTBString = "Happy_Thought_Button ID= " + this.hTB_ID +
-	 			" Media_ID_Tag= " + this.media_ID_Tag +
-	 			" Flagged= " + Boolean.toString(this.flagged) +
-	 			" Rating= " + this.rating +
-	 			" Date Time Executed= " + this.dT_Executed;
-	 			
-	 	return hTBString;
-	 	
-	 }
 	
 	/**
 	 * creates an array of images to display
