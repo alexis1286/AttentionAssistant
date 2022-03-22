cf0043310219aeb1a6e133d6e0105294103f0383
jmitchel <jmitc001@odu.edu>
2022-03-22 16:53:17 -0400

    EyeMovementTracker rotation of the image captured, and limiter.

--- src/main/java/AttentionAssistant/EyeMovementTracker.java
+++ src/main/java/AttentionAssistant/EyeMovementTracker.java
@@ -6,17 +6,22 @@
 //To be implemented by Justus and Evan
 package AttentionAssistant;
 
-
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.objdetect.Objdetect;
 import org.opencv.videoio.VideoCapture;
+
 import nu.pattern.OpenCV;
+
 
 /**
  * CLASS IS NOT COMPLETED YET!!!!
@@ -73,21 +78,22 @@ public class EyeMovementTracker {
 	 * Start tracking for the EyeMovementTracker
 	 */
 	public void startTracking() {
-	/**
-	 * Still implementing -jmitchel2
-	 * Code will print out Whether the user is looking at the screen or not to the command line 
-	 * every time a users face is recognized
-	 * 
-	 * currently ends after 10 seconds of tracking'
-	 * 
-	 * Interestingly, The first frame for myself is always false. -jmitchel
-	 * -(Added a method to grab the boolean every 5 frames to counteract false negatives.)
-	 * 
-	 */	
+
+		/**
+		 * Still implementing -jmitchel2
+		 * Code will print out Whether the user is looking at the screen or not to the command line 
+		 * every time a users face is recognized
+		 * 
+		 * currently ends after 10 seconds of tracking'
+		 * 
+		 * Interestingly, The first frame for myself is always false. -jmitchel
+		 * -(Added a method to grab the boolean every 5 frames to counteract false negatives.)
+		 * 
+		 */	
 		//load OpenCV shared library
 	  	OpenCV.loadLocally();
-		
-	  	//new capture object
+	  	
+		//new capture object
 		VideoCapture capture= new VideoCapture(0);
         
 		//If the camera is opened
@@ -103,6 +109,9 @@ public class EyeMovementTracker {
     		//grabs the time 10 seconds in the future
     		long endTime = System.nanoTime() + TimeUnit.NANOSECONDS.convert(10L, TimeUnit.SECONDS);
     		
+    		//grabs the time 4 minutes 45 seconds in the future
+    		//long endTime = System.nanoTime() + TimeUnit.NANOSECONDS.convert(4L, TimeUnit.MINUTES) + TimeUnit.NANOSECONDS.convert(45L, TimeUnit.SECONDS);
+    		
     		//while the system clock is not greater than the end time
     		while (System.nanoTime()< endTime){
     			
@@ -116,13 +125,20 @@ public class EyeMovementTracker {
     				Boolean isFaceDetectedFrame= this.continueTracking(capture);
 
     				// prints to the command line whether a face has been detected (true) or if none are found (false)
-    				System.out.println("Looking at screen: " + isFaceDetectedFrame);
-    	        
+    				//System.out.println("Looking at screen: " + isFaceDetectedFrame);
+    				
+    				//Timer to reduce the frames
+    				long waitTime = System.nanoTime() + TimeUnit.NANOSECONDS.convert(1L, TimeUnit.SECONDS);
+    				
     				//if a face is detected in any of the 5 frames set isFaceDetected to true 
     				if (isFaceDetectedFrame == true) {
     					isFaceDetected=true;
     				}
-    			
+    				//Timer to reduce the frames
+	    				while (System.nanoTime() < waitTime)
+	    				{
+	    					
+	    				}
     			//end of for loop
     			}
     			
@@ -144,7 +160,7 @@ public class EyeMovementTracker {
     	    System.out.println("I have finished EyeMovemtTracking\nEyeMovementScore= " + eyeMovementScoreCalculator);
     	    
     	    //Print number of frames5 and number of facesFound
-    	    //System.out.println("Frames5 = " + frames5 + "\nFacesFound = " + facesFound);
+    	    System.out.println("Frames5 = " + frames5 + "\nFacesFound = " + facesFound);
     	    
     	    //Sets eyeMovementScore
     	    this.eyeMovementScore= (int)eyeMovementScoreCalculator;		
@@ -152,43 +168,92 @@ public class EyeMovementTracker {
     	    //close the camera down
     		capture.release();
 		}
-
 		//if the camera will not open
 		else {
 			System.out.println("Camera Error!");
 			//set eyeMovementScore to -1
 			this.eyeMovementScore= -1;
 		}
-	
 	}
 	
+	/**
+	 * This function is used to capture an image on from the users web camera and return true if a face is detected.
+	 * 
+	 * This function has some issues with it:
+	 * -1. A well lit room is required to detect a users face.
+	 * 
+	 * @param VideoCapture
+	 * @return boolean
+	 */
 	public boolean continueTracking(VideoCapture capture) {
-		//creates a boolean object to return default false
-		boolean isFaceDetected=false;
-		//Creates Mat Object
-		Mat mat = new Mat();
-		//Grabs a from from capture and stores it in mat
+		//Creates a Mat Image
+		Mat mat= new Mat();
+		//Grabs a image from capture and stores it in mat
 		capture.read(mat);
-		//creates facesDetected object
-		MatOfRect facesDetected = new MatOfRect();
 		//creates cascadeClassifier object
 		CascadeClassifier cascadeClassifier = new CascadeClassifier();
-		//set minfacesize on image
-		int minFaceSize= Math.round(mat.rows()*0.1f);
 		//load the cascadeClassifier from resources folder
 		cascadeClassifier.load("./src/main/resources/Eye_Movement_Tracking/haarcascade_frontalface_alt.xml");
-		//Detects faces from the mat image
-        cascadeClassifier.detectMultiScale(mat, facesDetected, 1.1, 3, Objdetect.CASCADE_SCALE_IMAGE, new Size(minFaceSize, minFaceSize), new Size());			
-        //stores an array of faces from the detectmultiscale
-        Rect[] facesArray = facesDetected.toArray();
-        
-        //I know this warning is here -jmitchel, 
-        //just need to check the array to make sure it isn't null if it is null
-        for(@SuppressWarnings("unused")Rect face : facesArray) {
-        	//if a face is detected it will change to true
-        	isFaceDetected=true;
-        }
-        //return true or false based on if a face was detected.
-        return isFaceDetected;
+		//Creates rotatedMat Object
+		Mat rotatedMat =new Mat(2, 3, CvType.CV_32FC1);
+		//Creates new Point Object this will be the center of the image
+		Point center = new Point(mat.cols() / 2, mat.rows() / 2);
+		//set minfacesize on image
+		int minFaceSize= Math.round(mat.rows()*0.1f);
+		//Rotation angle of the image
+		for (int i=0; i <7; i++) {
+			//sets the angle of the rotation
+			int angle = 0;
+			//center
+			if (i==0){
+				angle = 0;
+			} 
+			//clockwise 20
+			else if (i==1){
+				angle = 20;
+			}
+			//clockwise 40
+			else if (i==2){
+				angle = 40;
+			}
+			//clockwise 60
+			else if (i==3){
+				angle = 60;
+			}
+			//counterclockwise 60
+			else if (i==4){
+				angle = 300;
+			}
+			//counterclockwise 40
+			else if (i==5){
+				angle = 320;
+			}
+			//counterclockwise 20
+			else if (i==6){
+				angle = 340;
+			}	
+			//Creates new mat labeled destination with mats size
+			Mat destination = new Mat(mat.rows(), mat.cols(), mat.type());
+			//creates facesDetected object
+			MatOfRect facesDetected = new MatOfRect();
+			//rotated mat by an angle and store it into rotatedMat
+			rotatedMat= Imgproc.getRotationMatrix2D(center, angle, 1);
+			//make the rotation and store it into destination
+			Imgproc.warpAffine(mat, destination, rotatedMat, mat.size());
+			//detect if any faces are present using the cascadeClassifier
+			cascadeClassifier.detectMultiScale(destination, facesDetected, 1.3, 3, Objdetect.CASCADE_SCALE_IMAGE, new Size(minFaceSize, minFaceSize), new Size());			
+			//stores an array of faces from the detectmultiscale
+			Rect[] facesArray = facesDetected.toArray();
+			//just need to check the array to make sure it isn't null if it is null
+			for(@SuppressWarnings("unused") Rect face : facesArray) {
+				//if a face is detected it will change to true
+				return true;
+			}
+		}
+    //return true or false based on if a face was detected.
+    return false;
 	}
+	
 }
+
+	
